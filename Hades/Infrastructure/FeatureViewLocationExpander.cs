07e99e81003de46a0748e8b6db9fec7686bb5ae6
Ankur Sheel <ankur.sheel@gmail.com>
2018-08-21 16:48:05 +1200

    add feature infrastructure files

---
+++ Hades/Infrastructure/FeatureViewLocationExpander.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Razor;
+
+namespace Hades.Infrastructure
+{
+	public class FeatureViewLocationExpander : IViewLocationExpander
+	{
+		public void PopulateValues(ViewLocationExpanderContext context)
+		{
+		}
+
+		public IEnumerable<string> ExpandViewLocations(ViewLocationExpanderContext context, IEnumerable<string> viewLocations)
+		{
+			if (context == null)
+			{
+				throw new ArgumentNullException(nameof(context));
+			}
+
+			if (viewLocations == null)
+			{
+				throw new ArgumentNullException(nameof(viewLocations));
+			}
+
+			// {0} - Action Name
+			// {1} - Controller Name
+			// {2} - Area Name
+			// Replace normal view location entirely
+			yield return "/Features/{1}/{0}.cshtml";
+			yield return "/Features/Shared/{0}.cshtml";
+		}
+	}
+}
