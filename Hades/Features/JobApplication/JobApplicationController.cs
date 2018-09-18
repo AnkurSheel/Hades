23b03a0902942e681f52659ca8ba3005a0a8c1cc
Ankur Sheel <ankur.sheel@gmail.com>
2018-09-18 21:46:57 +1200

    add controller and view

---
+++ Hades/Features/JobApplication/JobApplicationController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hades.Features.JobApplication
+{
+    public class JobApplicationController : Controller
+    {
+        [Route("~/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
